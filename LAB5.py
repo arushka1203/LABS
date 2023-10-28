388ac892d40f650dcdf592cf0ee7827feda428c2
arushka1203 <146517011+arushka1203@users.noreply.github.com>
2023-10-28 13:02:49 +0600

    Add files via upload

---
+++ LAB5.py
@@ -0,0 +1,6 @@
+user_input = input("Enter a string with whitespaces: ")
+# Convert the input string to lowercase and create a list of characters
+char_list = [char.lower() for char in user_input]
+
+print("Created list is:")
+print(char_list)
