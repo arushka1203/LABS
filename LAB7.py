6876377480f83ff852a32b7299036be265e81cbc
arushka1203 <146517011+arushka1203@users.noreply.github.com>
2023-11-25 12:08:00 +0600

    Update LAB7.py

--- LAB7.py
+++ LAB7.py
@@ -1,98 +1,156 @@
-def read_file_line_by_line(filename):
-    with open(filename, 'r') as file:
-        for line in file:
-            print(line)
+def task1():
+    print(f"Task 1")
+    data = open("text1.txt")
+    for i, line in enumerate(data):
+        print(line, end='')
+    data.close()
+
+
+task1()
 
 import random
 
-def read_random_line(filename):
-    with open(filename, 'r') as file:
-        lines = file.readlines()
-        random_line = random.choice(lines)
-        print(random_line)
+
+def task2():
+    print(f"\n\nTask 2")
+    data = open("text1.txt")
+    lines = data.readlines()
+    print(lines[random.randint(0, len(lines))])
 
 
-def count_uppercase_characters(filename):
-    uppercase_count = 0
-    with open(filename, 'r') as file:
-        for line in file:
-            for char in line:
-                if char.isupper():
-                    uppercase_count += 1
-    return uppercase_count
+task2()
 
 
-def count_lines_not_starting_with_d(filename):
-    lines_without_d = 0
-    with open(filename, 'r') as file:
-        for line in file:
-            if not line.startswith('D'):
-                lines_without_d += 1
-    return lines_without_d
+def task3():
+    print(f"\n\nTask 3")
+    data = open("text1.txt").read()
+    uppercaseCount = 0
+    for i in range(0, len(data)):
+        if data[i].isupper():
+            uppercaseCount += 1
+    print(uppercaseCount)
 
 
-def count_words_ending_with_f(filename):
-    words_ending_with_f = 0
-    with open(filename, 'r') as file:
-        for line in file:
-            words = line.split()
-            for word in words:
-                if word.lower().endswith('f'):
-                    words_ending_with_f += 1
-    return words_ending_with_f
+task3()
 
 
-def count_words_all_none(filename):
-    all_count = 0
-    none_count = 0
-    with open(filename, 'r') as file:
-        for line in file:
-            words = line.split()
-            for word in words:
-                if word.lower() == 'all':
-                    all_count += 1
-                elif word.lower() == 'none':
-                    none_count += 1
-    return all_count, none_count
+def task4():
+    print(f"\n\nTask 4")
+    data = open("text1.txt")
+    lines = data.readlines()
+    cnt = 0
+    for line in range(0, len(lines)):
+        if lines[line][0].upper() != 'D':
+            cnt += 1
+    print(cnt)
 
 
-def count_word_frequencies(filename):
-    word_counts = {}
-    with open(filename, 'r') as file:
-        for line in file:
-            words = line.split()
-            for word in words:
-                word_counts[word.lower()] = word_counts.get(word.lower(), 0) + 1
-    return word_counts
+task4()
 
 
-def find_longest_word(filename):
-    longest_word = ''
-    with open(filename, 'r') as file:
-        for line in file:
-            words = line.split()
-            for word in words:
-                if len(word) > len(longest_word):
-                    longest_word = word
-    return longest_word
+def task5():
+    print(f"\n\nTask 5")
+    data = open("text1.txt")
+    lines = data.readlines()
+    cnt = 0
+    for i in range(0, len(lines)):
+        line = lines[i]
+        if line[len(line) - 1] == '\n':
+            if line[len(line) - 2].upper() == 'F':
+                cnt += 1
+        else:
+            if line[len(line) - 1].upper() == 'F':
+                cnt += 1
+    print(cnt)
 
 
-def replace_b_with_j(filename):
-    corrected_content = ''
-    with open(filename, 'r') as file:
-        for line in file:
-            corrected_line = line.replace('B', 'J')
-            corrected_content += corrected_line
-    return corrected_content
+task5()
 
-def count_characters_a_b(filename):
-    a_count = 0
-    b_count = 0
-    with open(filename, 'r') as file:
-        for line in file:
-            for char in line.lower():
-                if char == 'a':
-                    a_count += 1
-                elif char == 'b':
-                    b_count += 1
-    return a_count, b_count
+
+def task6():
+    print(f"\n\nTask 6")
+    data = open("text1.txt")
+    lines = data.readlines()
+    cnt = 0
+    for i in range(0, len(lines)):
+        line = " " + lines[i].lower() + " "
+        cnt += line.count(" all ") + line.count(" none ") + line.count(" all\n") + line.count(" none\n")
+    print(cnt)
+
+
+task6()
+
+
+def task7():
+    print(f"\n\nTask 7")
+    data = open("text1.txt")
+    punctuation = ".?!,;:-_'\" "
+    lines = data.readlines()
+    words = [i.lower().split() for i in lines]
+    for i in range(len(words)):
+        for j in range(len(words[i])):
+            if words[i][j][len(words[i][j]) - 1] in punctuation:
+                words[i][j] = words[i][j][:-1]
+    wordsDic = {}
+    for i in words:
+        for j in i:
+            if j in wordsDic:
+                wordsDic[j] += 1
+            else:
+                wordsDic[j] = 1
+    print(wordsDic)
+
+
+task7()
+
+
+def task8():
+    print(f"\n\nTask 8")
+    data = open("text1.txt")
+    lines = data.readlines()
+    words = [i.lower().split() for i in lines]
+    punctuation = ".?!,;:-_'\" "
+    for i in range(len(words)):
+        for j in range(len(words[i])):
+            if words[i][j][len(words[i][j]) - 1] in punctuation:
+                words[i][j] = words[i][j][:-1]
+    longestWord = words[0][0]
+    for i in words:
+        for j in i:
+            if len(j) > len(longestWord):
+                longestWord = j
+    print(longestWord)
+
+
+task8()
+
+
+def task9():
+    print(f"\n\nTask 9")
+    data = open("text1.txt").read()
+    data = data.replace('B', 'J')
+    data = data.replace('b', 'j')
+    print(data)
+
+
+task9()
+
+
+def task10():
+    print(f"\n\nTask 10")
+    data = open("text1.txt")
+    lines = data.readlines()
+    cntA = 0
+    cntB = 0
+    for i in range(0, len(lines)):
+        line = " " + lines[i].lower() + " "
+        cntA += line.count('a')
+        cntB += line.count('b')
+    print(f"a={cntA}, b={cntB}")
+
+
+task10()
+
+   
+             
+       
